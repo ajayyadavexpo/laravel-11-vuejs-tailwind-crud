643726cb55a753ff16f81e84931005363645d123
Ajay yadav <36995310+ajayyadavexpo@users.noreply.github.com>
2025-01-13 10:52:08 +0530

    Add files via upload

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,6 +10,6 @@ class Post extends Model
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
 
-    protected $fillable = ['title','content'];
+    protected $fillable = ['title','content','image'];
     
 }
